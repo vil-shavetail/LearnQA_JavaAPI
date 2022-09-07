f8b4f044d999030570267c13394a145400a92221
vil-shavetail <demid.ivan@gmail.com>
2022-09-07 21:24:14 +0300

    Second Commit
    Modify HelloWorldTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,14 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld() {
-        System.out.println("Hello world from LearnQA_JavaAPI!");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
