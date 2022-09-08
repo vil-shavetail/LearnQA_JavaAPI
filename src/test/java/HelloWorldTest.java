bb44dfcdbfab4319c80d39eca1f4b95b88f3189a
vil-shavetail <demid.ivan@gmail.com>
2022-09-08 17:39:13 +0300

    Fourth commit
    Updated the testGetTextRequest method

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -17,6 +17,6 @@ public class HelloWorldTest {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
-        response.prettyPrint();
+        response.body().print();
     }
 }
