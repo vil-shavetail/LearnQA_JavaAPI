4edb7952a6137853b22e8a94e28963a3cbfb7260
vil-shavetail <demid.ivan@gmail.com>
2022-09-14 13:25:30 +0300

    Lesson 2 point 6
    Added the testCheckTypeWithPostAndSetParamsByBodyAsString() method.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -78,4 +78,15 @@ public class HelloWorldTest {
 
         response.print();
     }
+
+    @Test
+    public void testCheckTypeWithPostAndSetParamsByBodyAsString() {
+        Response response = RestAssured
+                .given()
+                .body("param1=value1&param2=value2")
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+
+        response.print();
+    }
 }
