648142fe724245bee376862fd78dea3199d1b510
vil-shavetail <demid.ivan@gmail.com>
2022-09-14 13:37:00 +0300

    Lesson 2 point 6
    Added the testCheckTypeWithPostAndSetParamsByBodyAsJSONByMap method.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -99,4 +99,18 @@ public class HelloWorldTest {
 
         response.print();
     }
+
+    @Test
+    public void testCheckTypeWithPostAndSetParamsByBodyAsJSONByMap() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("param1", "value1");
+        body.put("param2", "value2");
+        Response response = RestAssured
+                .given()
+                .body(body)
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+
+        response.print();
+    }
 }
