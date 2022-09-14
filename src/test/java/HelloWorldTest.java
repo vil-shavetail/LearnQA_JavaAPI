74bfc20fcbfb4f9aabf25242b07478201cb1d976
vil-shavetail <demid.ivan@gmail.com>
2022-09-14 12:02:53 +0300

    Updated HelloWorldTest
    Added the testRestAssuredSetParams method

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,10 @@
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
@@ -17,6 +21,21 @@ public class HelloWorldTest {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
+
         response.body().print();
+    }
+
+    @Test
+    public void testRestAssuredSetParams() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "John Rambo");
+
+        Response response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+
+        response.prettyPrint();
     }
 }
