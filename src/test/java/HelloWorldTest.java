97dab9133fcf568ab6cb01465c42837e09cf152e
vil-shavetail <demid.ivan@gmail.com>
2022-09-14 13:19:09 +0300

    Lesson 2 point 6
    Added the testCheckTypeWithGetAndQueryParam method.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -66,4 +66,16 @@ public class HelloWorldTest {
 
         response.prettyPrint();
     }
+
+    @Test
+    public void testCheckTypeWithGetAndQueryParam() {
+        Response response = RestAssured
+                .given()
+                .queryParam("param1","value1")
+                .queryParam("param2", "value2")
+                .get("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+
+        response.print();
+    }
 }
