93b95cc0ae5fb98f0d2be28f88b42dbd0e2103d9
vil-shavetail <demid.ivan@gmail.com>
2022-09-17 21:35:14 +0300

    Lesson 2 Ex9
    Added some fixes and the testGetSecretPassword method

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -73,7 +73,7 @@ public class HelloWorldTest {
     public void testCheckTypeWithGetAndQueryParam() {
         Response response = RestAssured
                 .given()
-                .queryParam("param1","value1")
+                .queryParam("param1", "value1")
                 .queryParam("param2", "value2")
                 .get("https://playground.learnqa.ru/api/check_type")
                 .andReturn();
@@ -239,7 +239,7 @@ public class HelloWorldTest {
         String responseCookie = responseForGet.getCookie("auth_cookie");
 
         Map<String, String> cookies = new HashMap<>();
-        if(responseCookie != null) {
+        if (responseCookie != null) {
             cookies.put("auth_cookie", responseCookie);
         }
 
@@ -264,7 +264,7 @@ public class HelloWorldTest {
         List<Map<String, String>> responseList = response.getJsonObject("messages");
         Map<String, String> secondStringMap = responseList.get(1);
         System.out.println("The text of the second message is: " + "\"" + secondStringMap.get("message") + "\"");
-        System.out.println("The timestamp of the second message is: " + "\"" + secondStringMap.get("timestamp") +"\"");
+        System.out.println("The timestamp of the second message is: " + "\"" + secondStringMap.get("timestamp") + "\"");
     }
 
     @Test
@@ -310,7 +310,7 @@ public class HelloWorldTest {
             statusCode = response.getStatusCode();
             quantityOfTheRedirects++;
             System.out.println("Status code is: " + statusCode);
-            if(statusCode != 200) {
+            if (statusCode != 200) {
                 System.out.println("The redirect URL is: " + redirectUrl);
             }
 
@@ -355,6 +355,50 @@ public class HelloWorldTest {
             System.out.println("The status is: " + status);
             String result = thirdResponse.get("result");
             System.out.println("The result is: " + result);
+        }
+    }
+
+    @Test
+    public void testGetSecretPassword() {
+        String login = "super_admin";
+        String[] splashData = {"password", "123456", "123456789", "12345678", "12345", "qwerty",
+                "abc123", "football", "1234567", "monkey", "111111", "letmein", "1234", "1234567890",
+                "dragon", "baseball", "sunshine", "iloveyou", "trustno1", "princess", "adobe123", "123123",
+                "welcome", "login", "admin", "qwerty123", "solo", "1q2w3e4r", "master", "666666", "photoshop",
+                "1qaz2wsx", "qwertyuiop", "ashley", "mustang", "121212", "starwars", "654321", "bailey",
+                "access", "flower", "555555", "passw0rd", "shadow", "lovely", "7777777", "michael", "!@#$%^&*",
+                "jesus", "password1", "superman", "hello", "charlie", "888888", "696969", "hottie", "freedom",
+                "aa123456", "qazwsx", "ninja", "azerty", "loveme", "whatever", "donald", "batman", "zaq1zaq1",
+                "Football", "000000", "123qwe"};
+
+        for (String splashDatum : splashData) {
+            Map<String, String> params = new HashMap<>();
+            params.put("login", login);
+            params.put("password", splashDatum);
+
+            Response getAuthCookieResponse = RestAssured
+                    .given()
+                    .body(params)
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
+
+            String auth_cookie = getAuthCookieResponse.getCookie("auth_cookie");
+
+            Map<String, String> cookies = new HashMap<>();
+            cookies.put("auth_cookie", auth_cookie);
+
+            Response checkCookieResponse = RestAssured
+                    .given()
+                    .cookies(cookies)
+                    .get("https://playground.learnqa.ru/ajax/api/check_auth_cookie")
+                    .andReturn();
+
+            String responseMessage = checkCookieResponse.asString();
+            if (!responseMessage.contains("You are NOT authorized")) {
+                System.out.println("The valid password for the \"super_admin\" user is: " + splashDatum);
+                System.out.println("The response message is: " + responseMessage);
+                break;
+            }
         }
     }
 }
