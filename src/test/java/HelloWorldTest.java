7dc3baf664c0780959865ebcc4c7f55a9a04eef8
vil-shavetail <demid.ivan@gmail.com>
2022-09-21 23:46:28 +0300

    Lesson 3 point 2
    Added ParametrizedTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,6 +3,8 @@ import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.List;
@@ -418,5 +420,25 @@ public class HelloWorldTest {
                 .get("https://playground.learnqa.ru/api/map2")
                 .andReturn();
         assertEquals(404, response.statusCode(), "Unexpected status code");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "John", "Pete"})
+    public void testHelloWorldWithoutName(String name) {
+        Map<String, String> queryParams = new HashMap<>();
+
+        if(name.length() >0) {
+            queryParams.put("name", name);
+        }
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(queryParams)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        String expectedName = (name.length() > 0) ? name : "someone";
+        assertEquals("Hello, " + expectedName, answer, "The answer is not expected");
+
     }
 }
