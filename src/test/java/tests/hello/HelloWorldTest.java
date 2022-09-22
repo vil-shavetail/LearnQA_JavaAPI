dd33ef4248d358e51d58819ad029b878dec9623b
vil-shavetail <demid.ivan@gmail.com>
2022-09-22 22:01:51 +0300

    Lesson 3 Ex13
    Added the testAssertUserAgent method.

--- src/test/java/tests/hello/HelloWorldTest.java
+++ src/test/java/tests/hello/HelloWorldTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.*;
@@ -488,5 +490,29 @@ public class HelloWorldTest {
                 assertTrue(headers.asList().get(i).getValue().length() > 0);
             }
         }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 | platform: Mobile, browser: No, device: Android",
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1 | platform: Mobile, browser: Chrome, device: iOS",
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html) | platform: Googlebot, browser: Unknown, device: Unknown",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0 | platform: Web, browser: Chrome, device: No",
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 | platform: Mobile, browser: No, device: iPhone"
+    }, delimiter = '|')
+    public void testAssertUserAgent(String agent, String expectedValues) {
+        Response response = RestAssured
+                .given()
+                .header("User-Agent", agent)
+                .post("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .andReturn();
+
+        String actualPlatform = response.jsonPath().getString("platform");
+        String actualBrowser = response.jsonPath().getString("browser");
+        String actualDevice = response.jsonPath().getString("device");
+
+        String actualValues = "platform: " + actualPlatform + ", browser: " + actualBrowser + ", device: " + actualDevice;
+
+        assertEquals(expectedValues, actualValues);
     }
 }
