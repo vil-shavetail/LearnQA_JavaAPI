e9bdd7bd69b4f8b2d3806971c7263deac49eb820
vil-shavetail <demid.ivan@gmail.com>
2022-10-11 21:18:00 +0300

    Lesson 4 point 4 actual
    Added the UserEditTest class. Updated classes: Assertions, DataGenerator, UserRegisterTest.

--- src/test/java/lib/DataGenerator.java
+++ src/test/java/lib/DataGenerator.java
@@ -1,11 +1,39 @@
 package lib;
 
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DataGenerator {
 
     public static String getRandomEmail() {
         String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new java.util.Date());
         return "learnqa" + timestamp + "@example.com";
+    }
+
+    public static Map<String, String> getRegistrationData() {
+        Map<String, String> data = new HashMap<>();
+        data.put("email", DataGenerator.getRandomEmail());
+        data.put("password", "123");
+        data.put("username", "learnqa");
+        data.put("firstName", "learnqa");
+        data.put("lastName", "learnqa");
+
+        return data;
+    }
+
+    public static Map<String, String> getRegistrationData(Map<String, String> noneDefaultValues) {
+        Map<String, String> defaultValues = DataGenerator.getRegistrationData();
+
+        Map<String, String> userData = new HashMap<>();
+        String[] keys = {"email", "password", "username", "firstName", "lastName"};
+        for (String key: keys) {
+            if (noneDefaultValues.containsKey(key)) {
+                userData.put(key, noneDefaultValues.get(key));
+            } else {
+                userData.put(key, defaultValues.get(key));
+            }
+        }
+        return userData;
     }
 }
--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -18,10 +18,7 @@ public class UserRegisterTest extends BaseTestCase {
 
         Map<String, String> userData = new HashMap<>();
         userData.put("email", email);
-        userData.put("password", "123");
-        userData.put("username", "learnqa");
-        userData.put("firstName", "learnqa");
-        userData.put("lastName", "learnqa");
+        userData = DataGenerator.getRegistrationData(userData);
 
         Response responseCreateAuth = RestAssured
                 .given()
@@ -35,14 +32,7 @@ public class UserRegisterTest extends BaseTestCase {
 
     @Test
     public void testCreateUserSuccessfully() {
-        String email = DataGenerator.getRandomEmail();
-
-        Map<String, String> userData = new HashMap<>();
-        userData.put("email", email);
-        userData.put("password", "123");
-        userData.put("username", "learnqa");
-        userData.put("firstName", "learnqa");
-        userData.put("lastName", "learnqa");
+        Map<String, String> userData = DataGenerator.getRegistrationData();
 
         Response responseCreateAuth = RestAssured
                 .given()
